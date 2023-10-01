3de374c483b560019d0c2b49ad1a8e6205a06a47
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-10-01 07:15:59 +0200

    Create Builder.php

---
+++ src/Menu/Builder.php
@@ -0,0 +1,24 @@
+<?php // src/Menu/MenuBuilder.php
+
+namespace App\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class MenuBuilder
+{
+    private $factory;
+
+    public function __construct(FactoryInterface $factory)
+    {
+        $this->factory = $factory;
+    }
+
+    public function createMainMenu(RequestStack $requestStack)
+    {
+        $menu = $this->factory->createItem('root');
+        $menu->addChild('Home', ['route' => 'app_main']);
+
+        return $menu;
+    }
+}
