1bc2a5a95c7c53850725f493d311cf56fa35a246
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-10-01 05:53:06 +0200

    knplabs/menu-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
+    Knp\Bundle\MenuBundle\KnpMenuBundle::class => ['all' => true],
 ];
