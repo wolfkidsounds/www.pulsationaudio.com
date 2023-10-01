7a96b44aace8dcbae9293c9d723a714d50ba732e
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-10-01 04:03:36 +0200

    composer require symfony/webpack-encore-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Knp\Bundle\MenuBundle\KnpMenuBundle::class => ['all' => true],
     Nelmio\SecurityBundle\NelmioSecurityBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
