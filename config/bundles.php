59399c16b7c985f0cf67ba14997e4f690ed7247d
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-10-01 04:06:56 +0200

    composer require symfony/ux-turbo

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Nelmio\SecurityBundle\NelmioSecurityBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
 ];
