fc9f6bd6638d258c3b3f485345ff5396e6dee8bc
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-12-06 04:35:46 +0100

    Update Builder.php

--- src/Menu/Builder.php
+++ src/Menu/Builder.php
@@ -17,7 +17,16 @@ class MenuBuilder
     public function createMainMenu(RequestStack $requestStack)
     {
         $menu = $this->factory->createItem('root');
-        $menu->addChild('Home', ['route' => 'app_index']);
+        $menu->addChild('About', ['route' => 'app_index']);
+        $menu->addChild('Portfolio', ['route' => 'app_index']);
+        $menu->addChild('Projects', ['route' => 'app_index']);
+        $menu->addChild('Music', ['route' => 'app_index']);
+        $menu->addChild('Sound', ['route' => 'app_index']);
+        $menu->addChild('Mastering', ['route' => 'app_index']);
+        $menu->addChild('Label', ['route' => 'app_index']);
+        $menu->addChild('Web', ['route' => 'app_index']);
+        $menu->addChild('Samples', ['route' => 'app_index']);
+        $menu->addChild('Contact', ['route' => 'app_index']);
 
         return $menu;
     }
