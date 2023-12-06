f4ec4dadd558bd2d3bc49fc753fc444a246182b9
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-12-06 04:35:40 +0100

    Update Styling

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,21 @@
+// Import Controllers
 import './bootstrap.js';
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
 
-// any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+// Import Node Modules JS
+import '@popperjs/core';
+import 'bootstrap';
+import 'select2/dist/js/select2.js';
+
+// Import Node Modules CSS
+import '@fortawesome/fontawesome-free/css/all.css';
+import 'select2/dist/css/select2.css';
+import 'select2-bootstrap-5-theme/dist/select2-bootstrap-5-theme.css';
+
+// Import Custom JS
+import './js/app.js';
+
+// Import Custom SCSS
+import './scss/app.scss';
+
+// Import Custom CSS
+import './css/app.css';
