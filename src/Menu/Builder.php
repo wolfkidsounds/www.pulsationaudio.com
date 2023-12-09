da0ed3c8d22ddef62839372152271ca12d688419
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-12-09 23:00:44 +0100

    Update Builder.php

--- src/Menu/Builder.php
+++ src/Menu/Builder.php
@@ -17,15 +17,19 @@ class MenuBuilder
     public function createMainMenu(RequestStack $requestStack)
     {
         $menu = $this->factory->createItem('root');
-        $menu->addChild('About', ['route' => 'app_index']);
         $menu->addChild('Portfolio', ['route' => 'app_index']);
         $menu->addChild('Projects', ['route' => 'app_index']);
-        $menu->addChild('Music', ['route' => 'app_index']);
-        $menu->addChild('Sound', ['route' => 'app_index']);
+        $menu->addChild('div_1', ['divider' => true, 'extras' => ['divider' => true]]);
         $menu->addChild('Mastering', ['route' => 'app_index']);
+        $menu->addChild('Music', ['route' => 'app_index']);
+        $menu->addChild('Sound Design', ['route' => 'app_index']);
+        $menu->addChild('div_2', ['divider' => true, 'extras' => ['divider' => true]]);
         $menu->addChild('Label', ['route' => 'app_index']);
         $menu->addChild('Web', ['route' => 'app_index']);
+        $menu->addChild('div_3', ['divider' => true, 'extras' => ['divider' => true]]);
         $menu->addChild('Samples', ['route' => 'app_index']);
+        $menu->addChild('div_4', ['divider' => true, 'extras' => ['divider' => true]]);
+        $menu->addChild('About', ['route' => 'app_index']);
         $menu->addChild('Contact', ['route' => 'app_index']);
 
         return $menu;
