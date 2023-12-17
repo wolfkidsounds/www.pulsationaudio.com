18663f4d022f3d0d02e2ad7d3905210a0ebe76f9
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-12-17 08:56:06 +0100

    Create admin.js

---
+++ assets/admin.js
@@ -0,0 +1,15 @@
+// NODE MODULES JS
+
+// NODE MODULES CSS
+
+// FONTS
+//import './fonts/fonts.css';
+
+// JS
+//import './js/admin.js';
+
+// SCSS
+//import './scss/app.scss';
+
+// CSS
+import './css/admin.css';
