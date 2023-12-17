3b4e128067d9510868293f728a97e9731eb44f89
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-12-17 22:27:28 +0100

    Update PageCrudController.php

--- src/Controller/Admin/PageCrudController.php
+++ src/Controller/Admin/PageCrudController.php
@@ -3,10 +3,11 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Page;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class PageCrudController extends AbstractCrudController
 {
@@ -15,14 +16,9 @@ class PageCrudController extends AbstractCrudController
         return Page::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        yield TextField::new('Title');
+        yield TextField::new('slug');
     }
-    */
 }
