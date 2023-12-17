9ec918a24aa0bd8ed7fd5179504761a9d71fab0d
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-12-17 08:56:10 +0100

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -4,12 +4,9 @@ import './bootstrap.js';
 // Import Node Modules JS
 import '@popperjs/core';
 import 'bootstrap';
-import 'select2/dist/js/select2.js';
 
 // Import Node Modules CSS
 import '@fortawesome/fontawesome-free/css/all.css';
-import 'select2/dist/css/select2.css';
-import 'select2-bootstrap-5-theme/dist/select2-bootstrap-5-theme.css';
 
 // Import Custom JS
 import './js/app.js';
