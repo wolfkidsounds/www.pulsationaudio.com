0e5ac32ad288c482d12d19892cb12d40c19c4a38
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-12-17 22:27:31 +0100

    Update ContentController.php

--- src/Controller/ContentController.php
+++ src/Controller/ContentController.php
@@ -2,31 +2,45 @@
 
 namespace App\Controller;
 
-use App\Repository\CategoryRepository;
 use App\Repository\ContentRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\OptionsRepository;
+use App\Repository\CategoryRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+#[Route('/content')]
 class ContentController extends AbstractController
 {
-    #[Route('/content/{slug}', name: 'app_content')]
-    public function content(string $slug, CategoryRepository $categoryRepository, ContentRepository $contentRepository): Response
+    #[Route('/portfolio', name: 'app_content_portfolio')]
+    public function portfolio(OptionsRepository $optionsRepository): Response
     {
-        $category = $categoryRepository->findOneBy(['slug' => $slug]);
-        $posts = $contentRepository->findBy(['Category' => $category]);
-        $title = $category->getName();
+        $option = $optionsRepository->findOneBy(['Name' => 'Portfolio']);
+        $posts = $option->getContents();
+        $title = $option->getName();
 
-        $template = strtolower($category->getTemplate()->getName()) . '.html.twig';
-
-        return $this->render('_template/' . $template, [
+        return $this->render('_template/grid.html.twig', [
             'title' => $title,
             'posts' => $posts,
             'controller_name' => 'ContentController',
         ]);
     }
 
-    #[Route('/content/{slug}/{id]', name: 'app_content_single')]
+    #[Route('/projects', name: 'app_content_projects')]
+    public function projects(OptionsRepository $optionsRepository): Response
+    {
+        $option = $optionsRepository->findOneBy(['Name' => 'Projects']);
+        $posts = $option->getContents();
+        $title = $option->getName();
+
+        return $this->render('_template/grid.html.twig', [
+            'title' => $title,
+            'posts' => $posts,
+            'controller_name' => 'ContentController',
+        ]);
+    }
+
+    #[Route('/{slug}/{id]', name: 'app_content_single')]
     public function single(string $slug, int $id, CategoryRepository $categoryRepository, ContentRepository $contentRepository): Response
     {
         $category = $categoryRepository->findOneBy(['Category' => $slug]);
@@ -38,6 +52,22 @@ class ContentController extends AbstractController
         return $this->render('_template/single/' . $template, [
             'title' => $title,
             'post' => $post,
+            'controller_name' => 'ContentController',
+        ]);
+    }
+
+    #[Route('/{slug}', name: 'app_content')]
+    public function content(string $slug, CategoryRepository $categoryRepository, ContentRepository $contentRepository): Response
+    {
+        $category = $categoryRepository->findOneBy(['slug' => $slug]);
+        $posts = $contentRepository->findBy(['Category' => $category]);
+        $title = $category->getName();
+
+        $template = strtolower($category->getTemplate()->getName()) . '.html.twig';
+
+        return $this->render('_template/' . $template, [
+            'title' => $title,
+            'posts' => $posts,
             'controller_name' => 'ContentController',
         ]);
     }
