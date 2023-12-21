4c29de23c796b20f31bb747b5dd005d5a4c11591
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-12-21 07:03:47 +0100

    Update Content.php

--- src/Entity/Content.php
+++ src/Entity/Content.php
@@ -3,8 +3,6 @@
 namespace App\Entity;
 
 use App\Repository\ContentRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -19,27 +17,18 @@ class Content
     #[ORM\ManyToOne(inversedBy: 'Contents')]
     private ?Category $Category = null;
 
-    #[ORM\ManyToMany(targetEntity: Tag::class, inversedBy: 'Contents')]
-    private Collection $Tags;
-
     #[ORM\Column(length: 255)]
     private ?string $Title = null;
 
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $Thumbnail = null;
 
-    #[ORM\Column(type: Types::TEXT, nullable: true)]
-    private ?string $Description = null;
-
-    #[ORM\ManyToOne(inversedBy: 'Contents')]
-    private ?Options $Options = null;
-
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $Url = null;
 
     public function __construct()
     {
-        $this->Tags = new ArrayCollection();
+
     }
 
     public function getId(): ?int
@@ -55,30 +44,6 @@ class Content
     public function setCategory(?Category $Category): static
     {
         $this->Category = $Category;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Tag>
-     */
-    public function getTags(): Collection
-    {
-        return $this->Tags;
-    }
-
-    public function addTag(Tag $tag): static
-    {
-        if (!$this->Tags->contains($tag)) {
-            $this->Tags->add($tag);
-        }
-
-        return $this;
-    }
-
-    public function removeTag(Tag $tag): static
-    {
-        $this->Tags->removeElement($tag);
 
         return $this;
     }
@@ -103,30 +68,6 @@ class Content
     public function setThumbnail(string $Thumbnail): static
     {
         $this->Thumbnail = $Thumbnail;
-
-        return $this;
-    }
-
-    public function getDescription(): ?string
-    {
-        return $this->Description;
-    }
-
-    public function setDescription(?string $Description): static
-    {
-        $this->Description = $Description;
-
-        return $this;
-    }
-
-    public function getOptions(): ?Options
-    {
-        return $this->Options;
-    }
-
-    public function setOptions(?Options $Options): static
-    {
-        $this->Options = $Options;
 
         return $this;
     }
