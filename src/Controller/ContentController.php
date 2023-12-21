a3e7ac4792091c626701d379a9116e763d4a38ef
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-12-21 07:03:41 +0100

    Update ContentController.php

--- src/Controller/ContentController.php
+++ src/Controller/ContentController.php
@@ -12,50 +12,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 #[Route('/content')]
 class ContentController extends AbstractController
 {
-    #[Route('/portfolio', name: 'app_content_portfolio')]
-    public function portfolio(OptionsRepository $optionsRepository): Response
-    {
-        $option = $optionsRepository->findOneBy(['Name' => 'Portfolio']);
-        $posts = $option->getContents();
-        $title = $option->getName();
-
-        return $this->render('_template/grid.html.twig', [
-            'title' => $title,
-            'posts' => $posts,
-            'controller_name' => 'ContentController',
-        ]);
-    }
-
-    #[Route('/projects', name: 'app_content_projects')]
-    public function projects(OptionsRepository $optionsRepository): Response
-    {
-        $option = $optionsRepository->findOneBy(['Name' => 'Projects']);
-        $posts = $option->getContents();
-        $title = $option->getName();
-
-        return $this->render('_template/grid.html.twig', [
-            'title' => $title,
-            'posts' => $posts,
-            'controller_name' => 'ContentController',
-        ]);
-    }
-
-    #[Route('/{slug}/{id}', name: 'app_content_single')]
-    public function single(string $slug, int $id, CategoryRepository $categoryRepository, ContentRepository $contentRepository): Response
-    {
-        $category = $categoryRepository->findOneBy(['slug' => $slug]);
-        $post = $contentRepository->findOneBy(['id' => $id]);
-        $title = $post->getTitle();
-
-        $template = strtolower($category->getName()) . '.html.twig';
-
-        return $this->render('_template/single/' . $template, [
-            'title' => $title,
-            'post' => $post,
-            'controller_name' => 'ContentController',
-        ]);
-    }
-
     #[Route('/{slug}', name: 'app_content')]
     public function content(string $slug, CategoryRepository $categoryRepository, ContentRepository $contentRepository): Response
     {
