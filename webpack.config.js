2d6d2b67712dda6d65d6f061a3cb4a1abb4498a3
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-12-21 07:04:07 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ Encore
     .splitEntryChunks()
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
-    .enableStimulusBridge('./assets/controllers.json')
+    //.enableStimulusBridge('./assets/controllers.json')
 
     // will require an extra script tag for runtime.js
     // but, you probably want this, unless you're building a single-page app
